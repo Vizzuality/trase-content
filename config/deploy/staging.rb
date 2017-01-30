b5d32037245c640d4be40362528383f77dae27a7
Tiago Garcia <tiagojsag@gmail.com>
2017-01-30 15:24:16 +0100

    Add staging deploy env; set assets to https

---
+++ config/deploy/staging.rb
@@ -0,0 +1,5 @@
+server 'staging.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
+set :ssh_options, {
+    forward_agent: true
+}
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

eb370159c9a63d48e3c1cfdec2ec17fc85fa8c60
Tiago Santos <santos.tiago@gmail.com>
2017-02-02 12:26:08 +0100

    Assets precompilation testing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.autoload_paths += %W(#{Rails.application.config.root}/app/models/ckeditor)
 Rails.application.config.assets.precompile += Ckeditor.assets
 Rails.application.config.assets.precompile += %w( ckeditor/* )
-Rails.application.config.autoload_paths += %W(#{Rails.application.config.root}/app/models/ckeditor)

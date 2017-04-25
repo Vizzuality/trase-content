ff387bc224774fa77c21e0260fc2112fe8f0b58f
Tiago Garcia <tiagojsag@gmail.com>
2017-04-25 12:51:52 +0200

    Handle no twitter credentials case

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :stage, :staging
-set :branch, :develop
+set :branch, :master
+
 server 'staging.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, {
     forward_agent: true
 }
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
